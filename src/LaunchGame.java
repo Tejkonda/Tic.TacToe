4183de491893d5278a62b4e38b3241150bc12a74
DELL <kondaveetitejaswi99@gmail.com>
2023-06-18 15:12:17 -0500

    Done Checking all possibilities to win the game row, col,

--- src/LaunchGame.java
+++ src/LaunchGame.java
@@ -2,10 +2,12 @@ public class LaunchGame {
     public static void main(String args[]){
         TicTacToe T= new TicTacToe();
         T.displayBoard();
-        T.placeMark(2,1,'X');
-        T.placeMark(1,1,'O');
-        T.placeMark(0,2,'O');
+        T.placeMark(0,0,'X');
+        T.placeMark(1,1,'X');
+        T.placeMark(2,2,'X');
         T.displayBoard();
         System.out.println(T.checkRowWin());
+        System.out.println(T.checkColWin());
+        System.out.println(T.checkDiagonalWin());
     }
 }
